439bd839037c904717a500573809301c829f0319
Rauni <rakangas@microsoft.com>
2019-07-20 22:40:35 -0700

    Add project files.

---
+++ Rx_Tracker_v3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rx_Tracker_v3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
