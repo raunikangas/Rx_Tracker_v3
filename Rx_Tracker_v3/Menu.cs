fb77f02f363506b89a9e0078e7aabe0b7fa38373
Rauni <rakangas@microsoft.com>
2019-07-20 22:58:15 -0700

    Initial commit. Creating files for Menu and Processing

---
+++ Rx_Tracker_v3/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rx_Tracker_v3
+{
+    public class Menu
+    {
+        public int ConsoleIntProcessing(string consoleOutput)
+        {
+            while(true)
+            {
+                Console.Write(consoleOutput);
+                try
+                {
+                    return Convert.ToInt32(Console.ReadLine());
+                }
+                catch(FormatException)
+                {
+
+                }
+            }
+        }
+
+        
+
+
+        public void MainMenu()
+        {
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Rx_Tracker_v3/Processing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rx_Tracker_v3
+{
+    public static class Processing
+    {
+        public static void ListPatients()
+        {
+
+        }
+
+
+
+
+
+    }
+}
